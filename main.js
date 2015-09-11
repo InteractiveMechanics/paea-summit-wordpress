4860f7ae1c573d2e589a6d4bbf8a4ed9f6df2b8c
Christina Deemer <cahdeemer@gmail.com>
2015-09-11 14:02:28 -0400

    homepage styles except some assets

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-$( document ).ready(function() {
+$( window ).load(function() {
 
 	/* GRID / ISOTOPE */
+
 
 	$('.grid').isotope({
 	  // options
 	  itemSelector: '.grid-item',
 	  layoutMode: 'masonry'
 	});
+
+	$('.grid').isotope(
+  masonry: {
+    columnWidth: 220
+  }
+});
 
 });
