1766cf8b84172d4fdfe2c0b6d1175d6b63ef8e61
Christina Deemer <cahdeemer@gmail.com>
2015-09-15 13:01:03 -0400

    testing theme files

---
+++ wp-content/themes/paea_theme/footer.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage PAEA_Summit 
+ * @since PAEA Summit 1.0
+ */
+?>
+
+
+		</div><!-- #main -->
+		<footer>
+			 <div class="container">
+    <div class="row email-sign-up">
+      <div class="col-sm-12">
+        <h4 class="hidden-xs hidden-sm hidden-md">Want to find out how you can be involved in the summit?</h4>
+        <h2 class="hidden-xs hidden-sm hidden-md">Sign up for future summit updates</h2>
+        <h2 class="visible-xs visible-sm visible-md">Sign up for summit updates</h2>
+      </div>
+      <div class="col-sm-12">
+        <form action="//paeaonline.us5.list-manage.com/subscribe/post?u=c1336de511800dd633b850bd9&id=66efaba916" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div class="input-group">
+            <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="joe@website.com">
+            <span class="input-group-btn">
+               <div style="position: absolute; left: -5000px;"><input type="text" name="b_c1336de511800dd633b850bd9_66efaba916" tabindex="-1" value=""></div>
+              <input class="btn btn-default" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
+            </span>
+          </div>
+        </form>
+      </div>
+    </div> <!-- end row -->
+  </div> <!-- end container -->
+
+  <div class="bottom_divider"></div>
+    <div class="container">
+      <div class="row">
+        <ul>
+          <li>&copy; Physicians Assistant Education Association (PAEA)</li>
+          <li class="hidden-xs hidden-sm"><a href="emailto:stakeholdersummit@paeaonline.org">stakeholdersummit@paeaonline.org</a></li>
+          <li><a href="">Frequently Asked Questions</a></li>
+        </ul>
+      </div>
+    </div> <!-- end container -->
+		</footer>
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+
+	<!-- SCRIPTS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>	
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.1/isotope.pkgd.js"></script>
+  <script src="main.js"></script>
+
+</body>
+</html>
---
+++ wp-content/themes/paea_theme/header.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage PAEA_Summit
+ * @since PAEA Summit 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title>PAEA 2016 Stakeholder Summit</title>
+
+		<!-- STYLESHEETS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  	<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/3aadffeb-0b44-4d31-a24a-0f039bb78dd3.css"/>
+  	<link rel="stylesheet" href="style.css">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+	<header>
+			<nav class="navbar navbar-default">
+        <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="PAEA logo"</a>
+            <h1 class="sr-only">PAEA 2016 Stakeholder Summit</h1>
+          </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="about.html">About the Summit</a></li>
+            <li><a href="attendees.html">For attendees</a></li>
+            <li><a href="schedule.html">Schedule</a></li>
+            <li><a href="discussion.html">Join the Discussion</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </header>
